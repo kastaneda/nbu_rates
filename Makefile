173000e199ddb696c3ae6512447817b52e4bb617
Dmitry Kolesnikov <kastaneda@gmail.com>
2019-12-28 06:16:16 +0200

    Join downloaded data to CSV and ODS

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 TARGETS := $(shell php list_targets.php)
 
-all: $(TARGETS)
+nbu_rates.csv: $(TARGETS)
+	php join_dataset.php > $@
+
+nbu_rates.ods: nbu_rates.csv
+	# https://wiki.openoffice.org/wiki/Documentation/DevGuide/Spreadsheets/Filter_Options
+	libreoffice --headless --infilter="CSV:44,34,76,1,1,1/5" --convert-to ods $<
 
 %.json:
 	wget "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=$*&json" -O $@ || rm $@
-
-.PHONY: all
---
+++ join_dataset.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+
+$currencies = ['USD', 'EUR', 'GBP', 'PLN', 'CZK'];
+
+// CSV header
+echo 'Date,' . join(',', $currencies) . PHP_EOL;
+
+$files = array_filter(
+    scandir(__DIR__, SCANDIR_SORT_ASCENDING),
+    function ($filename) {
+        return preg_match('/^\d{8}.json$/', $filename);
+    }
+);
+
+foreach ($files as $filename) {
+    $dataset = json_decode(file_get_contents($filename), true);
+
+    // Date
+    $date_y = substr($filename, 0, 4);
+    $date_m = substr($filename, 4, 2);
+    $date_d = substr($filename, 6, 2);
+    echo sprintf('%s-%s-%s', $date_y, $date_m, $date_d);
+
+    // Rates for all required currencies
+    foreach ($currencies as $currency) {
+        echo ',';
+
+        // Search
+        foreach ($dataset as $row) {
+            if ($row['cc'] == $currency) {
+                echo sprintf('%.7f', $row['rate']);
+            }
+        }
+    }
+
+    echo PHP_EOL;
+}
