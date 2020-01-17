abfe370ae9e5b055a0b448d0422755c3948afd35
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-01-17 12:13:16 +0200

    Load tomorrow rates after 16:00

--- list_targets.php
+++ list_targets.php
@@ -1,12 +1,17 @@
 #!/usr/bin/env php
 <?php
 
+$one_day = new DateInterval('P1D');
 $kiev_tz = new DateTimeZone('Europe/Kiev');
 $date_start = new DateTime('2007-01-01', $kiev_tz);
 $date_end = new DateTime(null, $kiev_tz);
 
-$everyday = new DateInterval('P1D');
-$period = new DatePeriod($date_start, $everyday, $date_end);
+$hour = (int) date('H');
+if ($hour >= 16) {
+    $date_end = $date_end->add($one_day);
+}
+
+$period = new DatePeriod($date_start, $one_day, $date_end);
 foreach ($period as $day) {
     echo 'data/' . $day->format('Ymd') . '.json' . PHP_EOL;
 }
